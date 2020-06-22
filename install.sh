a4426e538982b4b2236a8b20c9a78bc973d67aec
Jashaswimalya Acharjee <31704400+jash-maester@users.noreply.github.com>
2020-06-22 15:21:17 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/jash-maester/i3_custom-build.git 
+echo 'Copying files...'
 
-cd i3_custom-build
 cp .config/i3 ~/.config/ -rf && cp .config/polybar ~/.config/ -rf && cp .config/rofi ~/.config/ -rf
 
 echo 'DONE...'
